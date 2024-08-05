74a9759e27bc97306d7fe5d08fc5f53606137a91
junkf8d <93854615+junkf8d@users.noreply.github.com>
2024-08-05 20:32:43 +0900

    RIDDLE用スクリプト + 実行結果を追加

--- scripts/common.rb
+++ scripts/common.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'fileutils'
 require 'date'
+require 'nokogiri'
 
 CACHE_DIR = File.expand_path('../cache', __dir__)
 RESULT_DIR = File.expand_path('../results', __dir__)
@@ -14,7 +15,7 @@ def retrieve_and_cache(url, extension: '', cache: true)
   return File.read(path) unless cache
 
   uri = URI.parse(url)
-  uri_path = "#{uri.host}/#{uri.path}".gsub(/[\\:*?"<>|]/, '_').sub(%r{/$}, '')
+  uri_path = "#{uri.host}#{uri.path}".gsub(/[\\:*?"<>|]/, '_').sub(%r{/$}, '')
 
   path = File.join(CACHE_DIR, uri_path + extension)
   return File.read(path) if File.exist?(path)
@@ -23,4 +24,11 @@ def retrieve_and_cache(url, extension: '', cache: true)
 
   sleep rand(3.0..8.0) # 負荷がかかりすぎないよう数秒スリープ
   Net::HTTP.get(uri).tap { |d| File.write(path, d) }
+end
+
+def create_link_map(url_list)
+  url_list.map do |url|
+    key = URI.parse(url).hostname.split('.')[-2]
+    [key, url]
+  end.to_h
 end
---
+++ scripts/riddle.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'json'
+require_relative 'common'
+
+GROUP_NAME = 'RIDDLE'
+GROUP_SLUG = 'riddle'
+
+def get_liver_list
+  url = 'https://riddle.info/members/'
+  html = retrieve_and_cache(url, extension: '.html')
+  doc = Nokogiri::HTML.parse(html)
+  doc.css('.p-members__list a').map { |a| a['href'] }
+end
+
+def get_liver_detail(url)
+  html = retrieve_and_cache(url)
+  doc = Nokogiri::HTML.parse(html)
+
+  name = doc.at_css('h1').text.strip
+  urls = doc.css('.p-members__profile-sns a').map { |a| a[:href] }
+
+  { name: name, links: create_link_map(urls), tags: [GROUP_NAME] }
+end
+
+def main
+  puts " * [#{Time.now}] start retrieving #{GROUP_NAME} data..."
+
+  liver_list = get_liver_list
+  puts " * [#{Time.now}] parse liver list completed."
+
+  len = liver_list.length
+  result = liver_list.map.with_index(1) do |url, i|
+    puts format("   * [#{Time.now}] %03d/%03d (%.2f%%) parsing: %s", i, len, i.to_f / len, url)
+    get_liver_detail(url)
+  end
+
+  file_path = File.join(RESULT_DIR, "#{GROUP_SLUG}.json")
+  File.open(file_path, 'w') do |file|
+    file.write(JSON.pretty_generate(result))
+  end
+
+  puts " * [#{Time.now}] retrieving #{GROUP_NAME} data completed!"
+end
+
+puts main
