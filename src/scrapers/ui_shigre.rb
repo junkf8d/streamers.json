1dd5cc9607f965be848101a70377ef272d7a88a5
junkf8d <93854615+junkf8d@users.noreply.github.com>
2024-08-07 22:24:50 +0900

    しぐれういを追加

---
+++ src/scrapers/ui_shigre.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'json'
+require_relative '../common'
+
+GROUP_NAME = 'しぐれうい'
+GROUP_SLUG = 'ui_shigure'
+
+UPDATE_LIST = ARGV[0] == 'update_list'
+
+def main
+  puts " * [#{Time.now}] [#{GROUP_NAME}] Starting to retrieve data..."
+  url = 'https://5th.uishigure.com/'
+  html = retrieve_and_cache(url, extension: '.html', cache: !UPDATE_LIST)
+  doc = Nokogiri::HTML.parse(html)
+
+  urls = doc.css('.nav-snslinks a').map { |a| a['href'] }
+
+  name = 'しぐれうい'
+  result = [{ name: name, links: create_link_map(urls), tags: [GROUP_NAME], page: url }]
+
+  file_path = File.join(RESULT_DIR, "#{GROUP_SLUG}.json")
+  File.open(file_path, 'w') do |file|
+    file.write(JSON.pretty_generate(result))
+  end
+
+  puts " * [#{Time.now}] [#{GROUP_NAME}] Data retrieval completed!"
+end
+
+main
