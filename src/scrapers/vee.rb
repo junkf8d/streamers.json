b3b8e4f197e1185c051ba81ffa0dfff22d99ecb2
junkf8d <93854615+junkf8d@users.noreply.github.com>
2024-08-12 16:00:25 +0900

    VEEを追加

---
+++ src/scrapers/vee.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'json'
+require_relative '../common'
+
+GROUP_NAME = 'VEE'
+GROUP_SLUG = 'vee'
+
+MEMBER_LIST_URL = 'https://vee-official.jp/talent/'
+
+UPDATE_LIST = ARGV[0] == 'update_list'
+
+def get_streamer_list(url)
+  html = retrieve_and_cache(url, extension: '.html', cache: !UPDATE_LIST)
+  doc = Nokogiri::HTML.parse(html)
+  doc.css('.p-talent__list-item a').reject do |a|
+    a.parent.parent.parent['class'] == 'l-graduation'
+  end.map do |a|
+    a['href']
+  end
+end
+
+def get_streamer_detail(url)
+  html = retrieve_and_cache(url)
+  doc = Nokogiri::HTML.parse(html)
+
+  name = doc.at_css('.p-talent_info__name-text').text.strip
+  urls = doc.css('.p-talent_info_sns__list-item a').map { |a| a[:href] }.reject(&:empty?) # なんかURLが空のがある
+
+  { name: name, links: create_link_map(urls), tags: [GROUP_NAME].uniq, page: url }
+end
+
+def main
+  puts " * [#{Time.now}] [#{GROUP_NAME}] Starting to retrieve data..."
+
+  streamer_list = get_streamer_list(MEMBER_LIST_URL)
+  puts " * [#{Time.now}] [#{GROUP_NAME}] Parsing Streamer list completed."
+
+  len = streamer_list.length
+  result = streamer_list.map.with_index(1) do |url, i|
+    puts format("   * [#{Time.now}] [#{GROUP_NAME}] %03d/%03d (%.2f%%) parsing: %s", i, len, i.to_f / len, url)
+    get_streamer_detail(url)
+  end
+
+  file_path = File.join(RESULT_DIR, "#{GROUP_SLUG}.json")
+  File.open(file_path, 'w') do |file|
+    file.write(JSON.pretty_generate(result))
+  end
+
+  puts " * [#{Time.now}] [#{GROUP_NAME}] Data retrieval completed!"
+end
+
+main
